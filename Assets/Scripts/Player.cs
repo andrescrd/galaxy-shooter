6f83286c119b9572deec97552d51a0faee72b96d
andrescrd <c.andresricardo@gmail.com>
2021-02-27 00:26:38 -0300

    refactoring

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private Vector3 InitialPosition = new Vector3(0, 0, 0);
-    [SerializeField]
-    private float Speed = 5;
+    [SerializeField] private Vector3 initialPosition = new Vector3(0, 0, 0);
+    [SerializeField] private float speed = 5;
+    [SerializeField] private GameObject laserPrefab;
+    [SerializeField] private float fireRate = 0.5f;
+    [SerializeField] private Vector3 laserOffset = new Vector3(0, 0.8f, 0);
+
+    private float _nextFire;
 
     void Start()
     {
-        transform.position = InitialPosition;
+        transform.position = initialPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _nextFire)
+            FireLaser();
+    }
+
+    private void FireLaser()
+    {
+        _nextFire = Time.time + fireRate;
+        Instantiate(laserPrefab, transform.position + laserOffset, Quaternion.identity);
     }
 
     private void CalculateMovement()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalinput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(horizontalInput, verticalinput, 0);
-        transform.Translate(direction * Time.deltaTime * Speed);
+        var direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * (Time.deltaTime * speed));
 
-        Vector3 newPosition = new Vector3();
-        newPosition.y = Mathf.Clamp(transform.position.y, -3, 0);
-        newPosition.x = Mathf.Clamp(transform.position.x, -9, 9);
+        var newPosition = new Vector3
+        {
+            y = Mathf.Clamp(transform.position.y, -3, 0),
+            x = Mathf.Clamp(transform.position.x, -9, 9)
+        };
 
         transform.position = newPosition;
     }
