5ed0ddfbb9ba2d3d925c32895b9c6e45bb6fb7e1
andrescrd <c.andresricardo@gmail.com>
2021-02-27 01:28:15 -0300

    spawn manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject enemyContainer;
+    [SerializeField] private float timeToSpawn = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+    
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            var newPosition = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            var enemy = Instantiate(enemyPrefab, newPosition, Quaternion.identity);
+            enemy.transform.parent = enemyContainer.transform;
+
+            yield return new WaitForSeconds(timeToSpawn);
+        }
+    }
+}
